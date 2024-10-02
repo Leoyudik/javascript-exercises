1e49989fb040e06b0ef8f1f8770cac10b174d8a0
leoyudik <leoykana@gmail.com>
2024-10-02 08:36:40 -0300

    Exercicio 2 finalizado

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    let resultado = ''
+    if(num<0){
+        return 'ERROR';
+    }
+    for (let i =1; i <= num; i++){
+        resultado += string;
+    }
+    return resultado
 };
+
+
+console.log(repeatString('hey',3));
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
