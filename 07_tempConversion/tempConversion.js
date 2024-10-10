03dab382651ba0b5f8d6561abb11dba9b35fae08
leoyudik <leoykana@gmail.com>
2024-10-10 18:42:16 -0300

    Realizado sem muito problema. Ao usar o toFixed() o valor se torna string. Por isso é necessário utilizar o parseFloat().

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {  //F to Celcius
+  let formulaCelcius = (f - 32) * (5/9);
+  celciusRounded = parseFloat(formulaCelcius.toFixed(1));
+  return celciusRounded;
+
 };
 
-const convertToFahrenheit = function() {
+//convertToCelsius(32);
+
+const convertToFahrenheit = function(c) {
+  let formulaFahrenheit = c * (9/5) + 32;
+  fahrenheitRounded = parseFloat(formulaFahrenheit.toFixed(1));
+  return fahrenheitRounded;
+
 };
+
+//convertToFahrenheit(73.2);
 
 // Do not edit below this line
 module.exports = {
