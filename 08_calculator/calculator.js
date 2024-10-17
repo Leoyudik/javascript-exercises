a33a6d67c9f4672df507ede3254636686eefc727
leoyudik <leoykana@gmail.com>
2024-10-17 16:36:16 -0300

    Resolvido mas com if else para, facilitando valores como 0 e 1

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,65 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	formula = n1 + n2;
+  return formula;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  formula = n1 - n2;
+  return formula;
 };
 
-const sum = function() {
-	
+
+const sum = function(number) {
+  let resultado = 0;
+
+  for (let i = 0; i < number.length; i++){
+    resultado += number[i];
+  }
+    return resultado;
+
 };
 
-const multiply = function() {
+const multiply = function(number) {
+  let resultado = 1;
+
+  if (number.length === 0){
+    resultado = 0;
+    return resultado;
+  } else {
+    for (let i = 0; i < number.length; i++){
+      resultado = resultado * number[i];
+  }
+ }return resultado;
 
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let resultado = 1;
+
+  for(let i = 0; i < exp; i++)
+  resultado *= base;
+  return resultado;
+  
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+	let resultado = 1;
+
+  if(num === 0){
+    return resultado;
+  } else if ( num === 1 ){
+    return resultado;
+  } else{ 
+    for (let i = num; i > 1; i--){
+      resultado = resultado * i;
+      console.log(resultado, i);
+    }
+    return resultado ;
+  }
 };
+
+factorial(10); //3628800
 
 // Do not edit below this line
 module.exports = {
