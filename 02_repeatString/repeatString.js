820a7a0b8ea64fbd8d75eedc4f4eb6d8d8bb8431
Leoyudik <leoykana@gmail.com>
2025-02-28 08:31:48 -0300

    Exer 02 feito

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let resultado = '';
+    if(num <0){
+        return 'ERROR';
+    }else{
+        for (i=1; i<=num; i++){
+            resultado +=string;
+        } return resultado;
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
