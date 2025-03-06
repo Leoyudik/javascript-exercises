f9b7aede2fcb30c7a9a778e15dee6a945f390a58
Leoyudik <leoykana@gmail.com>
2025-03-06 16:18:45 -0300

    Reverse String finalizado

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+
+const reverseString = function(palavra) {
+    const letras = Array.from(palavra).reverse().join("");
+    return letras;
+
 
 };
+
+reverseString('hello there')
 
 // Do not edit below this line
 module.exports = reverseString;
