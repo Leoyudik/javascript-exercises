101debd2ec69c3258ed3401d62ca760565928d54
Leoyudik <leoykana@gmail.com>
2025-03-17 15:50:57 -0300

    Temp Conversion feio utilizando Const e if..

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,33 @@
-const convertToCelsius = function() {
+//F to C
+//x °F ≘ (x − 32) × ⁠ 5 / 9 ⁠ °C
+const convertToCelsius = function(temp) {
+  const tempCelsius = (temp - 32) * (5/9);
+  const tempRound = tempCelsius.toFixed(1);
+  const tempNumber = Number(tempRound);
+
+  if (tempCelsius % 2 ===0){
+    return tempCelsius;
+  }
+  return tempNumber;
+
+};
+convertToCelsius(100)
+
+
+//C to F
+//x °C ≘ (x × ⁠ 9 / 5 ⁠ + 32) °F
+const convertToFahrenheit = function(temp) {
+  const tempFahrenheit = (temp * 9 / 5 + 32);
+  const tempRound = tempFahrenheit.toFixed(1);
+  const tempNumber = Number(tempRound);
+
+  if (tempFahrenheit % 2 ===0){
+    return tempFahrenheit;
+  }
+  return tempNumber;
 };
 
-const convertToFahrenheit = function() {
-};
+
 
 // Do not edit below this line
 module.exports = {
