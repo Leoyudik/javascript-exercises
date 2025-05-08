4eff3f8f609498ccf1688f8967c018d9584a2593
Leoyudik <leoykana@gmail.com>
2025-05-08 17:25:55 -0300

    exercicio 09 Palindromes feito, utilizado join ao invés de toString

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    
+    //Retira toda a pont
+    const arrPontuationDeleted = 
+        Array.from(arr).map(pontuacao => pontuacao.replace(/[/?,.'"*-+! ""]/g, "")
+        .trim())
+        .filter(frase => frase !== "")
+        .map(word => word.toLowerCase())
+    const arrString = arrPontuationDeleted.join("");
+
+    let arrInvert = arrPontuationDeleted.slice().reverse().join("");
+    
+    console.log(arrString)
+    console.log(arrInvert)
+    console.log(arr)
+    return (arrString === arrInvert) ? true : false;
 
 };
+
+
+palindromes('racecar');
+
 
 // Do not edit below this line
 module.exports = palindromes;
