e2ccddcba1b2c87e1f397b29ca55769c1b70846f
Leoyudik <leoykana@gmail.com>
2025-05-21 15:45:44 -0300

    Fibo feito, utilizando looping com array)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n); // Converte String em Number
+    if (n === 0){
+        return 0;
+    }
+    if (n < 0){
+        return "OOPS";
+    }
+
+    const fib = [0,1];
+    
+    for (let i = 2; i <= n; i++){
+        fib[i] = fib[i-1] + fib[i -2];
+    }
+    return fib.at(-1);
 
 };
+console.log(fibonacci("0"))
+
+// (1, 1, 2, 3)
+
 
 // Do not edit below this line
 module.exports = fibonacci;
