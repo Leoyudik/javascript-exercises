5bf645b7389632340d284b1523815512bb9071b1
Leoyudik <leoykana@gmail.com>
2025-05-21 17:00:39 -0300

    GetTheTitle feito usando map.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+
+const getTheTitles = function(array) {
+    let titles = array.map(item => item.title);
+    return titles;
 
 };
+//['Book','Book2']
+// getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
